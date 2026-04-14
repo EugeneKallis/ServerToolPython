26ba12d5ed78677a82c920fbb1e98ce63267f19c
Eugene Kallivrousis <ekallivrousis@gmail.com>
2026-04-14 09:10:06 -0400

    fix: properly quote f-string in start.sh migration lock

--- backend/start.sh
+++ backend/start.sh
@@ -7,12 +7,12 @@ import sqlalchemy as sa
 from alembic import command
 from alembic.config import Config
 
-pg_host = os.environ.get("POSTGRES_HOST")
-pg_port = os.environ.get("POSTGRES_PORT")
-pg_user = os.environ.get("POSTGRES_USER")
-pg_password = os.environ.get("POSTGRES_PASSWORD")
-pg_db = os.environ.get("POSTGRES_DB")
-DATABASE_URL = f"postgresql://{pg_user}:{pg_password}@{pg_host}:{pg_port}/{pg_db}"
+pg_host = os.environ.get('POSTGRES_HOST')
+pg_port = os.environ.get('POSTGRES_PORT')
+pg_user = os.environ.get('POSTGRES_USER')
+pg_password = os.environ.get('POSTGRES_PASSWORD')
+pg_db = os.environ.get('POSTGRES_DB')
+DATABASE_URL = f'postgresql://{pg_user}:{pg_password}@{pg_host}:{pg_port}/{pg_db}'
 
 engine = sa.create_engine(DATABASE_URL)
 lock_id = 1234567  # Arbitrary unique lock ID per app
